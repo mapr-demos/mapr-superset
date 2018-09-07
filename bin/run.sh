fb47575c7da5bae2907e1371cbd2a7f1f9922e79
Michael Mamadilov <mmamadilov@cybervisiontech.com>
2018-09-07 15:10:48 +0300

    issue #1: run.sh changed to allow Drill, Hive nodes and WEB UI port configuration

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+DEFAULT_WEB_UI_PORT=8080
+
+# Check if 'WEB_UI_PORT' environment varaible set
+if [ ! -z ${WEB_UI_PORT+x} ]; then # WEB_UI_PORT exists
+    echo "Web UI port number: $WEB_UI_PORT"
+else
+    echo "WEB_UI_PORT environment variable is not set. Please set it and rerun. Defaulting to: $DEFAULT_WEB_UI_PORT"
+    WEB_UI_PORT=${DEFAULT_WEB_UI_PORT}
+fi
+
+# Check if 'DRILL_NODE' environment variable set
+if [ ! -z ${DRILL_NODE+x} ]; then # DRILL_NODE exists
+    echo "Drill node: $DRILL_NODE"
+else
+    echo 'DRILL_NODE environment variable is not set. Please set it and rerun.'
+    exit 1
+fi
+
+# Check if 'HIVE_NODE' environment variable set
+if [ ! -z ${HIVE_NODE+x} ]; then # HIVE_NODE exists
+    echo "Hive node: $HIVE_NODE"
+else
+    echo 'HIVE_NODE environment variable is not set. Please set it and rerun.'
+    exit 1
+fi
 
 fabmanager create-admin --app superset --username admin --password admin --firstname admin --lastname admin --email admin@fab.org
 
@@ -7,11 +32,11 @@ superset db upgrade
 superset load_examples
 superset init
 
-# TODO: sed nodenames
+sed -i -e "s=hive://node1=hive://$HIVE_NODE=g" /home/mapr/mapr-apps/mapr-superset/mapr-datasources
+sed -i -e "s=sadrill://node1=sadrill://$DRILL_NODE=g" /home/mapr/mapr-apps/mapr-superset/mapr-datasources
 
-superset import_datasources -p /opt/app-root/src/mapr-apps/mapr-superset/mapr-datasources
+superset import_datasources -p /home/mapr/mapr-apps/mapr-superset/mapr-datasources
 
 # TODO: find a way to import dashboards
-# TODO: configurable port number
 
-superset runserver -d -p 8081
+superset runserver -d -p ${WEB_UI_PORT}
